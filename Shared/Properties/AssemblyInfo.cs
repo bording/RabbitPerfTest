6a3596deb86f4fb988b976943ad5c0d693b31206
Brandon Ording <bording@gmail.com>
2016-08-15 16:32:47 -0400

    Initial project stubs

--- Shared/Properties/AssemblyInfo.cs
+++ Shared/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RabbitPerfTest")]
+[assembly: AssemblyTitle("Shared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RabbitPerfTest")]
+[assembly: AssemblyProduct("Shared")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25320517-febf-4127-bf3b-00220e477bf5")]
+[assembly: Guid("327463d3-09c8-4355-abcf-da5d76e7703a")]
 
 // Version information for an assembly consists of the following four values:
 //
