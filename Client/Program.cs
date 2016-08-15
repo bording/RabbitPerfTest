cb842e7337ba941a3a7d199637c79d4dd6f08897
Brandon Ording <bording@gmail.com>
2016-08-15 16:50:29 -0400

    Add client code

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NServiceBus;
+using Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,41 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Console.Title = "Client";
+
+            var config = new EndpointConfiguration("Client");
+            config.UseSerialization<JsonSerializer>();
+
+            config.SendOnly();
+
+            var transport = config.UseTransport<RabbitMQTransport>();
+            transport.Routing().RouteToEndpoint(typeof(PlaceOrder), "Server");
+
+            var endpoint = await Endpoint.Start(config);
+
+            var numberOfMessages = 200000;
+            await DirectSendAwaitAll(endpoint, numberOfMessages);
+
+            await endpoint.Stop();
+        }
+
+        static Task DirectSendAwaitAll(IEndpointInstance endpoint, int number)
+        {
+            var sends = new List<Task>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var send = endpoint.Send(new PlaceOrder { Id = Guid.NewGuid(), Product = "Message Body" });
+
+                sends.Add(send);
+            }
+
+            return Task.WhenAll(sends);
         }
     }
 }
