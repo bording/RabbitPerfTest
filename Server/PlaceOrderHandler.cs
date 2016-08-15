f6b17055973e1605bf389327e377b238d794ec5a
Brandon Ording <bording@gmail.com>
2016-08-15 17:15:36 -0400

    Add server code

---
+++ Server/PlaceOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context) => Task.FromResult(0);
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NServiceBus;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,33 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Console.Title = "Server";
+
+            var config = new EndpointConfiguration("Server");
+            config.UseSerialization<JsonSerializer>();
+
+            var prefetchCount = Convert.ToUInt16(ConfigurationManager.AppSettings["PrefetchCount"]);
+            var consumerCount = Convert.ToInt32(ConfigurationManager.AppSettings["ConsumerCount"]);
+            var concurrency = Convert.ToInt32(ConfigurationManager.AppSettings["Concurrency"]);
+
+            config.UseTransport<RabbitMQTransport>().PrefetchCount(prefetchCount).ConsumerCount(consumerCount);
+            config.UsePersistence<InMemoryPersistence>();
+            config.EnableInstallers();
+            config.SendFailedMessagesTo("error");
+
+            config.LimitMessageProcessingConcurrencyTo(concurrency);
+
+            var endpoint = await Endpoint.Start(config);
+
+            Console.WriteLine("Server started. Press any key to quit");
+            Console.ReadKey();
+
+            await endpoint.Stop();
         }
     }
 }
